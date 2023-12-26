de197bb1883b368d46a671ef26ae360eec16e49d
Maisarah <mamai29@test.com>
2023-12-26 18:13:12 +0800

    resend

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express')
+/*const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 
@@ -10,5 +10,27 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
    console.log(`Example app listening on port ${port}`)
-})
+})*/
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerJSDoc = require('swagger-jsdoc');
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'MyVMS API',
+            version: '1.0.0',
+        },
+    },
+    apis: ['./main/*.js'],
+}
+const swaggerSpec = swaggerJSDoc(options);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+
+
 
